fe451c06886247db360d0dacb48924058de3bb56
Ali Ghahraei <aligf94@gmail.com>
2021-06-13 20:13:09 -0500

    Add missing return types

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
-from pytest import CaptureFixture
+from subprocess import CalledProcessError
+from pytest import CaptureFixture, raises
 
 from typer_scripts.core import run, RunMode
 
 
 class TestRun:
     @staticmethod
-    def test_run_executes_command(capfd: CaptureFixture[str]):
+    def test_run_executes_command(capfd: CaptureFixture[str]) -> None:
         run(['echo', 'value'], RunMode.DEFAULT)
 
         assert 'value' in capfd.readouterr().out
 
     @staticmethod
-    def test_run_captures_output(capfd: CaptureFixture[str]):
+    def test_run_captures_output(capfd: CaptureFixture[str]) -> None:
         result = run(['echo', 'value'], RunMode.DEFAULT,
                      capture_output=True)
 
         assert 'value' in result.stdout.decode()
 
     @staticmethod
-    def test_run_prints_args_with_dry_run(capfd: CaptureFixture[str]):
+    def test_run_prints_args_with_dry_run(capfd: CaptureFixture[str]) -> None:
         run(['echo', 'value'], RunMode.DRY_RUN)
 
         assert "('echo', 'value')" in capfd.readouterr().out
 
     @staticmethod
-    def test_run_returns_expected_completed_process_with_dry_run():
+    def test_run_returns_expected_completed_process_with_dry_run() -> None:
         result = run(['echo', 'value'], RunMode.DRY_RUN)
 
         assert result.args == ('echo', 'value')
