9f3ec41207084cc2128968e54b5e6e257ceac261
Ali Ghahraei <aligf94@gmail.com>
2022-05-10 00:25:06 -0500

    Add config-apply to dbot

--- typer_scripts/main.py
+++ typer_scripts/main.py
@@ -6,6 +6,8 @@ from domestobot import (get_app as get_domestobot_app, get_root_dir,
 from typer_scripts.repos import app as repos_app
 from typer_scripts.typer_tools import App
 
+CONFIG_APPLY = 'config-apply'
+
 
 def get_app(path: str) -> typer.Typer:
     return get_domestobot_app(get_root_dir() / f'{path}.toml')
@@ -15,10 +17,12 @@ app = App()
 app.add_typer(get_app('maintenance'))
 app.add_typer(repos_app)
 app.add_typer(get_app('backup'))
+app.add_typer(get_app(CONFIG_APPLY))
 
 
 @app.callback(invoke_without_command=True)
 def main(ctx: typer.Context, dry_run: bool = dry_run_option):
     if ctx.invoked_subcommand is None:
-        for callback in get_groups_callbacks(app, ctx).values():
-            callback(dry_run)
+        for group_name, callback in get_groups_callbacks(app, ctx).items():
+            if group_name != CONFIG_APPLY:
+                callback(dry_run)
